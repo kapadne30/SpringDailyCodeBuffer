15f6debc95a9057fd950418d442dbc4d3685642d
kapadne30 <47255610+kapadne30@users.noreply.github.com>
2023-01-07 23:43:28 +0530

    Update new version

--- src/test/java/com/dailycodebuffer/springboot/tutorial/test1.java
+++ src/test/java/com/dailycodebuffer/springboot/tutorial/test1.java
@@ -1,10 +1,16 @@
 package com.dailycodebuffer.springboot.tutorial;
 
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 public class test1 {
     @Test
     public void testdemo(){
         System.out.println("hello java");
+
+        System.out.println("OKay");
+
     }
+
 }
