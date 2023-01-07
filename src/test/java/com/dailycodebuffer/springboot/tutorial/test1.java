fe1d0e3590536b13ff336e63d8ee37b046d31a3a
kapadne30 <47255610+kapadne30@users.noreply.github.com>
2023-01-07 11:54:19 +0530

    Hey Hi this is test1 file fot testing perpose

---
+++ src/test/java/com/dailycodebuffer/springboot/tutorial/test1.java
@@ -0,0 +1,10 @@
+package com.dailycodebuffer.springboot.tutorial;
+
+import org.junit.jupiter.api.Test;
+
+public class test1 {
+    @Test
+    public void testdemo(){
+        System.out.println("hello java");
+    }
+}
